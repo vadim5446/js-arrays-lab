eb45595d33cb4ff9620e12988708688cad78d6b1
Vadim Azimov <vadim@Vadims-MacBook-Air.local>
2024-07-01 15:32:53 -0400

    updated script.js

--- app.js
+++ app.js
@@ -68,5 +68,44 @@ for (let num of nums) {
 
 console.log('Exercise 11 result:', odds);
 // with chat gpt help
+/*
+Exercise 12: FizzBuzz with arrays
+
+1) Choose a method to iterate through the `nums` array. 
+
+2. As you loop, sort the numbers into new arrays based on the following rules:
+
+   - Push any number evenly divisible by 3 to an array called `fizz`.
+   - Push any number evenly divisible by 5 to an array called `buzz`.
+   - Push any number that is evenly divisible by 3 and 5 to an array called
+     `fizzbuzz`.
+
+   Note: A single number may meet more than one of the above rules. If it does,
+         it should be placed in multiple arrays. For example, the number `15`
+         will appear in the `fizz`, `buzz`, and `fizzbuzz` arrays.
+
+Complete Exercise 12 in the space below:
+*/
+const nums = [100, 5, 23, 15, 21, 72, 9, 45, 66, 7, 81, 90];
+
+let fizz = [];
+let buzz = [];
+let fizzbuzz = [];
+
+for (let i = 0; i < nums.length; i++) {
+    if (nums[i] % 3 === 0 && nums[i] % 5 === 0) {
+        fizzbuzz.push(nums[i]);
+    } else if (nums[i] % 3 === 0) {
+        fizz.push(nums[i]);
+    } else if (nums[i] % 5 === 0) {
+        buzz.push(nums[i]);
+    }
+}
+
+console.log('Exercise 12 Results:');
+console.log('  fizz:', fizz);
+console.log('  buzz:', buzz);
+console.log('  fizzbuzz:', fizzbuzz);
+
 
 
